490285054d07674a269849760736d0cbe94d6184
Devenik7 <nishanthreddy380@gmail.com>
2017-08-09 11:27:09 +0530

    testing commit from Android Studio itself

---
+++ app/src/main/java/com/devenik7/android/genericmusicplayer/PlayerBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.devenik7.android.genericmusicplayer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by nisha on 09-Aug-17.
+ */
+
+public class PlayerBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
